abdda63940bc45cfc5b54a9a438e77a3d9414463
JPMartins <jpmartins@nelogica.com.br>
2024-08-13 11:14:39 -0300

    feat: Constants & Assets

---
+++ constants/index.ts
@@ -0,0 +1,84 @@
+import ludovicoBanner from "../assets/projects/ludovicoBanner.webp";
+import polaroidSeBanner from "../assets/projects/polaroidSeBanner.webp";
+import zoomCloneBanner from "../assets/projects/zoomCloneBanner.webp";
+import personalWebsiteBanner from "../assets/projects/personalWebsiteBanner.webp";
+
+export const HERO_CONTENT = `Transforming the future of web development.`;
+
+export const ABOUT_TEXT = `My journey in development began with a curiosity about how things work, evolving into a career that forces me to learn and adapt continuously. During this time, I've worked on projects ranging from embedded systems for rockets to full-stack applications ready to be launched.`;
+export const ABOUT_TEXT_2 = `Currently, my focus is on building web applications with a design and business logic that meet any type of demand, reflecting a careful analysis of requirements so that everything is done the way it should be. In my spare time, I also create programming content and interact on forums to help other developers.`;
+export const ABOUT_TEXT_3 = `When I'm not coding, I'm usually playing tennis, going to the gym, hanging out with my girlfriend or with my family and a dog who knows how to smile to order snacks.`
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Software Assistant 2",
+    company: "Nelogica",
+    description: `Worked on the installation, configuration and management of partner brokers' databases. Developed Change Management (CM) to improve SQL proccedures and GIT workflows.`,
+    technologies: ["SQL", "Git", "Jira", "Python"],
+    link: "https://www.nelogica.com.br/",
+  },
+  {
+    year: "2021 - 2023",
+    role: "Undergraduate Research Student",
+    company: "LCC Uerj",
+    description: `Responsible for developing the website for the bachelor's degree course in Computer Science at UERJ. Also developed an application using the Python OpenCV library to identify the number of people in classrooms.`,
+    technologies: ["JavaScript", "Python", "Docker", "IoT", "ThingsBoard"],
+    link: "http://www.bcc.ime.uerj.br/",
+  },
+  {
+    year: "2023 - 2024",
+    role: "Freelancer",
+    company: "Ludovico Pet",
+    description: `Developed a Wordpress e-commerce site that contains all the logic needed to run a store, including authentication, product listing, shopping cart, category filtering and reviews.`,
+    technologies: ["Wordpress", "CSS", "JavaScript"],
+    link: "https://ludovicopet.com/",
+  },
+  {
+    year: "2022 - 2023",
+    role: "Systems Developer",
+    company: "GFRJ",
+    description: `Contributed to the development of algorithms for esp32 and arduino microcontrollers. Both embarked and successfully completed their tasks on a rocket with an apogee of 3 km.`,
+    technologies: ["C++", "Python"],
+    link: "http://www.gfrj.uerj.br/",
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Zoom Clone",
+    image: zoomCloneBanner,
+    description:
+      "Full-stack clone of the Zoom software. The clone contains authentication, call creation, device configuration and much more!",
+    technologies: ["Next.js 14", "TypeScript", "Tailwind CSS", "Clerk"],
+    link: "https://zoom-clone-nine-mu.vercel.app/sign-in?redirect_url=https%3A%2F%2Fzoom-clone-nine-mu.vercel.app%2F",
+  },
+  {
+    title: "Ludovico Pet E-Commerce",
+    image: ludovicoBanner,
+    description:
+      "Fully functional e-commerce with features such as product listing, shopping cart and user authentication.",
+    technologies: ["Wordpress", "CSS", "JavaScript"],
+    link: "https://ludovicopet.com/",
+  },
+  {
+    title: "Polaroid-se",
+    image: polaroidSeBanner,
+    description:
+      "A website where you can generate polaroids of different sizes by uploading any photo from your gallery.",
+    technologies: ["React", "JavaScript", "Tailwind CSS", "Vite"],
+    link: "https://polaroid-se.vercel.app/",
+  },
+  {
+    title: "Portfolio Website",
+    image: personalWebsiteBanner,
+    description:
+      "Personal website where I share my experiences, portfolio, services and some content.",
+    technologies: ["Next.js 14", "TypeScript", "Framer Motion"],
+    link: "",
+  },
+];
+
+export const CONTACT = {
+  email: "contact@joaopedrocosso.com",
+};
